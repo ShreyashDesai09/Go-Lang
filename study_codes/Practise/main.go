35d68eabb74b272187213cabc128b81367c88e64
Shreyash Desai <shreyashdesai31796@gmail.com>
2024-06-12 19:54:38 +0530

    golang

---
+++ study_codes/Practise/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num1 := 20
+	num2 := 10
+
+	fmt.Println(num1 , "\n")
+	fmt.Println(num2)
+
+	num := num1 + num2
+
+	fmt.Println(num)
+	
+}
--- study_codes/cards/main.go
+++ study_codes/cards/main.go
@@ -4,7 +4,17 @@ import "fmt"
 
 func main() {
 	// var card string = "Ace of Spades"
-	card := "Ace Of Spades"
-	
-	fmt.Println(card)
+	card := newcard()
+	fmt.Println("Card = ", card)
+
+	cards := deck{"Ace of heart", card}
+	cards = append(cards, "Six of Spade")
+
+	fmt.Println(cards)
+
+	cards.print()
+}
+
+func newcard() string {
+	return "Five of Dimonds"
 }
