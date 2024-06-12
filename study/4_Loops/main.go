65492f5bd437637c728ca3bef17fba6e840eaf9b
Shreyash Desai <shreyashdesai31796@gmail.com>
2024-06-12 23:40:13 +0530

    golang

---
+++ study/4_Loops/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	//long way
+	x := 0
+	for x < 5 {
+		fmt.Println("value of x is:",x)
+		x++
+	}
+
+	//short way
+	for i := 0;i<5;i++ {
+		fmt.Println("value of i:",i)
+	}
+	//value
+	names := []string {"SD","VP","MG"}
+
+	for i:= 0;i<len(names);i++ {
+		fmt.Println("Name is",names[i])
+	}
+
+	//with index and value
+	for index,value := range names {
+		fmt.Printf("Position at index %v is %v\n",index,value)
+	}
+
+	fmt.Println(names)
+}
