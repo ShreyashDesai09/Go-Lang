2cca19babd1513c551399e9447e3401c6f4b4fdf
Shreyash Desai <shreyashdesai31796@gmail.com>
2024-06-13 15:18:58 +0530

    golang

---
+++ study/5_bool_&_cond/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("Age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("Age is not less than 40")
+	}
+
+	names := []string{"SD", "VP", "MG", "VK", "SZ"}
+
+	for index, value := range names {
+		if index == 1 {
+			fmt.Println("Continue at pos\n", index)
+			continue
+		}
+		if index > 3 {
+			fmt.Println("Breaking at pos", index)
+			fmt.Printf("Value at beaking index %v is %v", index, value)
+			break
+		}
+		fmt.Printf("The value at pos %v is %v\n", index, value)
+	}
+}
